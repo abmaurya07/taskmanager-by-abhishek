f910eb98268178bd3fba88b59c45cd907b69b26a
Abhishek <abmaurya07@gmail.com>
2024-07-27 02:06:25 +0530

    dispatch added in login

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -5,18 +5,21 @@ import { useRouter } from 'next/navigation';
 import Cookie from 'js-cookie'; 
 import { FaLock, FaUser } from 'react-icons/fa'; 
 import {jwtDecode} from 'jwt-decode';
+import { useDispatch } from 'react-redux';
+import { setUsername } from '@/redux/UserData/userSlice';
 
 const LoginForm = () => { 
-  const [username, setUsername] = useState('');
+  const [username, setUsernameState] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-
+  const dispatch = useDispatch();
   const router = useRouter(); 
 
   const handleSubmit = async (e) => {
     e.preventDefault(); 
     try {
-      await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/login`, { username, password }, { withCredentials: true });
+      const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/login`, { username, password }, { withCredentials: true });
+      dispatch(setUsername)
       router.push('/dashboard');
     } catch (err) {
       setError('Invalid credentials');
@@ -56,7 +59,7 @@ const LoginForm = () => {
             placeholder="Username"
             required
             className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500"
-            onChange={(e) => setUsername(e.target.value)}
+            onChange={(e) => setUsernameState(e.target.value)}
           />
           <FaUser className="absolute top-3 right-3 text-gray-400" />
         </div>
