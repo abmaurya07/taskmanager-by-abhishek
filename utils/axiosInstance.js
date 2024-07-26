59fd1a8296b32d28341bcbaa9b9b91328b3138c5
Abhishek <abmaurya07@gmail.com>
2024-07-26 20:17:45 +0530

    Axios Instance updated

--- utils/axiosInstance.js
+++ utils/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true // Ensure cookies are sent with requests
 });
