89bcd16541493bfe5a2bf43c1c8c9369abb37038
Abhishek <abmaurya07@gmail.com>
2024-07-27 18:56:17 +0530

    Responsicve

--- components/UserSection.js
+++ components/UserSection.js
@@ -6,11 +6,10 @@ import ToolTip from './ToolTip';
 import { useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 
-
 const UserSection = () => {
   const router = useRouter();
   const [isScrolled, setIsScrolled] = useState(false);
-  const {username} = useSelector(state => state.user)
+  const { username } = useSelector(state => state.user);
 
   const handleLogout = () => {
     Cookie.remove('token'); // Remove the token from cookies
@@ -34,30 +33,26 @@ const UserSection = () => {
   }, []);
 
   return (
-    <div className={`fixed top-0 left-0 z-50 w-full mb-5 bg-white transition-shadow ${isScrolled ? 'shadow-lg' : ''}`}>
-    <div className={`flex items-center justify-between p-4 pr-5 pl-5`}>
-
-<h1 className="text-2xl lg:text-3xl font-extrabold mb-6 text-center lg:text-left text-gray-800">
-          Task App
-          <strong className="text-sm lg:text-md text-gray-700 ml-4 mb-6 text-center lg:text-left">
-         by Abhishek Maurya
-        </strong>
-        </h1>
-
-<div>
-<div className='flex'>
-
-        <h1 className="text-2xl font-bold text-gray-800 mr-4">Welcome {username}!</h1>
-        <ToolTip tooltip='Logout'>
-  
-        <AiOutlineLogout size={32} color='#DC3545' onClick={handleLogout} />
-
-        </ToolTip>
+    <div className={`fixed top-0 left-0 z-50 w-full bg-white transition-shadow ${isScrolled ? 'shadow-2xl' : ''}`}>
+      <div className={`flex flex-col lg:flex-row items-center justify-between p-2 pr-5 pl-5`}>
+        <div className="flex flex-col lg:flex-row items-center">
+          <h1 className="text-2xl lg:text-3xl font-extrabold text-center lg:text-left text-gray-800">
+            Task App
+          </h1>
+          <strong className="text-sm lg:text-md text-gray-700 mt-2 lg:mt-0 lg:ml-4">
+            by Abhishek Maurya
+          </strong>
         </div>
 
+        <div className="flex items-center mt-2 lg:mt-0">
+          <h1 className="text-lg lg:text-2xl font-bold text-gray-800 mr-2 lg:mr-4">
+            Welcome {username}!
+          </h1>
+          <ToolTip tooltip='Logout'>
+            <AiOutlineLogout size={24} color='#DC3545' onClick={handleLogout} />
+          </ToolTip>
         </div>
-       
-    </div>
+      </div>
     </div>
   );
 };
